390ba98b41bf9b05d0d243169b253107d0cd0a41
Julián Melero Hidalgo <julian@melerohidalgo.es>
2020-12-20 19:44:20 +0100

    MuchosAMuchos Schema

---
+++ LeerData/Instructor.cs
@@ -0,0 +1,10 @@
+namespace LeerData
+{
+    public class Instructor
+    {
+        public int InstructorId { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Grado { get; set; }
+    }
+}
